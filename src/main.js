bc8fe6e84a4cfc99993ff8013157972001a3ffb2
kikicom <kihwan0624@hotmail.com>
2021-07-16 17:36:02 +0900

    commit;

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
+import {
+  createApp
+} from 'vue'
+
 import App from './App.vue'
+import router from './router'
+import mixins from './mixins'
+import store from './store'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-Vue.config.productionTip = false
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mixin(mixins);
+app.use(store);
+app.use(VueSweetalert2);
+app.mount('#app');
+
+window.Kakao.init("d5cc1b5a3e8636bc01917b25cd43b870");
